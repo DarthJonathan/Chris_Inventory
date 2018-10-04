004d322dbd478bcb73f0c862c6ee4ad51803894d
Jonathan Hosea <jonathan.hosea@hotmail.com>
2018-10-04 11:20:45 +0700

    Migrations, Model , and Controllers

---
+++ database/migrations/2018_10_04_040441_create_sales_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateSalesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('sales', function (Blueprint $table) {
+            $table->increments('id');
+            $table->float('price')->nullable();
+            $table->integer('product_id')->nullable();
+            $table->integer('tax_invoice_id')->nullable();
+            $table->float('discount')->nullable();
+            $table->string('invoice_no')->nullable();
+            $table->dateTime('sales_date')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('sales');
+    }
+}
---
+++ database/migrations/2018_10_04_040518_create_purchases_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePurchasesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('purchases', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('tax_invoice_id')->nullable();
+            $table->integer('product_id')->nullable();
+            $table->string('invoice')->nullable();
+            $table->integer('quantity')->nullable();
+            $table->float('price')->nullable();
+            $table->float('discount')->nullable();
+            $table->date('purchase_date')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('purchases');
+    }
+}
