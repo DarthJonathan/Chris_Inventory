722365fcce1115f8f9bc346dec721a72f1015999
Jonathan Hosea <jonathan.hosea@hotmail.com>
2019-01-08 00:13:59 +0700

    Purchase table finished

--- resources/js/AdminScripts.js
+++ resources/js/AdminScripts.js
@@ -17,10 +17,10 @@ $(document).ready(function() {
                 'data': null,
                 render : function(data, type, row) {
                     return '<button class="btn btn-primary mr-2 edit-product" data-type="edit">' +
-                        '<i class="icon-pencil"></i>' +
+                            '<i class="icon-pencil"></i>' +
                         '</button>' +
                         '<button class="btn btn-danger delete-product" data-type="delete">' +
-                        '<i class="icon-trash"></i>' +
+                            '<i class="icon-trash"></i>' +
                         '</button>'
                 }
             },
@@ -70,14 +70,14 @@ $(document).ready(function() {
             {
                 'data': null,
                 render : function(data, type, row) {
-                    return '<button class="btn btn-primary mr-2 edit-product" data-type="edit">' +
-                        'Details' +
+                    return '<button class="btn btn-primary mr-2 edit-product" data-type="details">' +
+                            'Details' +
                         '</button>' +
                         '<button class="btn btn-primary mr-2 edit-product" data-type="edit">' +
-                        '<i class="icon-pencil"></i>' +
+                            '<i class="icon-pencil"></i>' +
                         '</button>' +
                         '<button class="btn btn-danger delete-product" data-type="delete">' +
-                        '<i class="icon-trash"></i>' +
+                            '<i class="icon-trash"></i>' +
                         '</button>'
                 }
             },
@@ -86,13 +86,15 @@ $(document).ready(function() {
     });
 
     $('#purchasesTable tbody').on('click', 'button', function() {
-        var data = productsTable.row( $(this).parents('tr') ).data();
+        var data = purchasesTable.row( $(this).parents('tr') ).data();
         var type = $(this).data('type');
 
         if(type === "edit") {
-            window.location.href = '/products/edit/' + data.id;
+            window.location.href = '/purchases/edit/' + data.id;
+        }else if(type === "details"){
+            window.location.href = '/purchases/details/' + data.id;
         }else{
-            axios.post('/products/delete', qs.stringify({id : data.id}))
+            axios.post('/purchases/delete', qs.stringify({id : data.id}))
                 .then(res => {
                     return res.data;
                 })
@@ -105,7 +107,64 @@ $(document).ready(function() {
                     console.log(res);
                 });
         }
+    });
 
+    //Sales Table
+    var salesTable = $('#salesTable').DataTable( {
+        "processing": true,
+        "serverSide": true,
+        "columns": [
+            {'data': 'invoice_id'},
+            {
+                'data': 'tax_invoice_id',
+                render: function(data,type,row) {
+                    if(data === null)
+                        return 'None';
+                    else
+                        return data;
+                }
+            },
+            {'data': 'transaction_date'},
+            {
+                'data': null,
+                render : function(data, type, row) {
+                    return '<button class="btn btn-primary mr-2 edit-product" data-type="details">' +
+                            'Details' +
+                        '</button>' +
+                        '<button class="btn btn-primary mr-2 edit-product" data-type="edit">' +
+                            '<i class="icon-pencil"></i>' +
+                        '</button>' +
+                        '<button class="btn btn-danger delete-product" data-type="delete">' +
+                            '<i class="icon-trash"></i>' +
+                        '</button>'
+                }
+            },
+        ],
+        "ajax": "/purchases/datatables"
+    });
+
+    $('#purchasesTable tbody').on('click', 'button', function() {
+        var data = purchasesTable.row( $(this).parents('tr') ).data();
+        var type = $(this).data('type');
+
+        if(type === "edit") {
+            window.location.href = '/purchases/edit/' + data.id;
+        }else if(type === "details"){
+            window.location.href = '/purchases/details/' + data.id;
+        }else{
+            axios.post('/purchases/delete', qs.stringify({id : data.id}))
+                .then(res => {
+                    return res.data;
+                })
+                .then(res => {
+                    if(res.success === true) {
+                        productsTable.ajax.reload();
+                    }else {
+
+                    }
+                    console.log(res);
+                });
+        }
     });
 
 });
--- resources/views/sales/overview.blade.php
+++ resources/views/sales/overview.blade.php
@@ -2,15 +2,13 @@
 
 @section('content')
 <div class="row">
-    <div class="col-lg-12">
-        <div class="card">
-            <div class="card-body">
-                <h1>Sales</h1>
-                <a href="{{ url('sales/new') }}" class="btn btn-primary float-right btn-lg">
-                    New Sale
-                </a>
-            </div>
-        </div>
+    <div class="col-lg-8">
+        <h2 class="c-grey-900 mT-10 mB-30">Sales</h2>
+    </div>
+    <div class="col-lg-4 text-right">
+        <a href="{{ url('sales/new') }}" class="btn btn-primary mt-2">
+            New Sale
+        </a>
     </div>
 </div>
 <div class="row">
@@ -28,27 +26,27 @@
                         </tr>
                     </thead>
                     <tbody>
-                        @foreach($sales as $count => $sale)
-                            <tr>
-                                <td>{{ $count+1 }}</td>
-                                <td>{{ $sale->invoice_id }}</td>
-                                <td>{{ \Carbon\Carbon::parse($sale->created_at)->format("D, d M Y") }}</td>
-                                <td>{{ $sale->taxInvoice != null ? $sale->taxInvoice->invoice_no : 'NONE' }}</td>
-                                <td>
-                                    <a href="/sales/detail/{{ $sale->id }}" class="btn btn-primary btn-block">Details</a>
-                                    <br>
-                                    <button class="btn btn-danger btn-block">Delete</button>
-                                </td>
-                            </tr>
-                        @endforeach
+                        {{--@foreach($sales as $count => $sale)--}}
+                            {{--<tr>--}}
+                                {{--<td>{{ $count+1 }}</td>--}}
+                                {{--<td>{{ $sale->invoice_id }}</td>--}}
+                                {{--<td>{{ \Carbon\Carbon::parse($sale->created_at)->format("D, d M Y") }}</td>--}}
+                                {{--<td>{{ $sale->taxInvoice != null ? $sale->taxInvoice->invoice_no : 'NONE' }}</td>--}}
+                                {{--<td>--}}
+                                    {{--<a href="/sales/detail/{{ $sale->id }}" class="btn btn-primary btn-block">Details</a>--}}
+                                    {{--<br>--}}
+                                    {{--<button class="btn btn-danger btn-block">Delete</button>--}}
+                                {{--</td>--}}
+                            {{--</tr>--}}
+                        {{--@endforeach--}}
                     </tbody>
                     <tfoot>
                         <tr>
-                            <td colspan="8">
-                                <div class="float-right">
-                                    {{ $sales->links() }}
-                                </div>
-                            </td>
+                            <th>No.</th>
+                            <th>Invoice No</th>
+                            <th>Sale Date</th>
+                            <th>Tax Invoice</th>
+                            <th style="width: 15%">Action</th>
                         </tr>
                     </tfoot>
                 </table>
