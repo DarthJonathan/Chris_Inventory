cfc913a89cdc8a8de373254f0223dd88c1982baf
Jonathan Hosea <jonathan.hosea@hotmail.com>
2019-10-08 10:25:45 +0700

    Update Dasboard

--- app/Http/Controllers/DashboardController.php
+++ app/Http/Controllers/DashboardController.php
@@ -2,6 +2,8 @@
 
 namespace App\Http\Controllers;
 
+use App\Transaction;
+use Carbon\Carbon;
 use Illuminate\Http\Request;
 
 class DashboardController extends Controller
@@ -11,6 +13,15 @@ class DashboardController extends Controller
      * @return \Illuminate\Contracts\View\Factory|\Illuminate\View\View
      */
     public function dashboard() {
-        return view('dashboard');
+        $total_transactions = Transaction::whereYear('transaction_date', Carbon::now())->count();
+        $total_transactions_last_year = Transaction::whereYear('transaction_date', Carbon::now()->subYear())->count();
+        $transaction_decrease = 100 - ($total_transactions/$total_transactions_last_year * 100);
+        $data = [
+            'total_transaction'   => Transaction::whereYear('transaction_date', Carbon::now())->count(),
+            'total_transaction_decrease'    => $transaction_decrease,
+            'total_sales'  => Transaction::where('type', 'Sales')->count(),
+            'total_purchases'  => Transaction::where('type', 'Purchases')->count()
+        ];
+        return view('dashboard', $data);
     }
 }
--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -10,14 +10,16 @@
                             <i class="mdi mdi-cube text-danger icon-lg"></i>
                         </div>
                         <div class="float-right">
-                            <p class="mb-0 text-right">Total Revenue</p>
+                            <p class="mb-0 text-right">Total Transactions This Year</p>
                             <div class="fluid-container">
-                                <h3 class="font-weight-medium text-right mb-0">$65,650</h3>
+                                <h3 class="font-weight-medium text-right mb-0">
+                                    {{ $total_transaction }}
+                                </h3>
                             </div>
                         </div>
                     </div>
                     <p class="text-muted mt-3 mb-0">
-                        <i class="mdi mdi-alert-octagon mr-1" aria-hidden="true"></i> 65% lower growth
+                        <i class="mdi mdi-alert-octagon mr-1" aria-hidden="true"></i> {{ round($total_transaction_decrease) }}% Transaction Decrease
                     </p>
                 </div>
             </div>
